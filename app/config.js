7e24b6d4206e8a2f2c11bb01318b9ef885391d83
gavinelliott <gavin@gavinelliott.co.uk>
2016-10-11 15:14:02 +0100

    Added in get help section

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Find pension contact details',
 
   // Default port that prototype runs on
   port: '3000',
